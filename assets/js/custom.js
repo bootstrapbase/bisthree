6b2c379b049b9a9fcd19c1c3b9e208e56c6ee182
sizif <imsirovic.ajdin@yahoo.com>
2017-03-17 12:46:42 +0000

    add datatables-1 (original), datatables-2 (customized), jumbotron-home, jumbotron-trazim-kupujem, datatables-fontawesome integration

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-    $('#example').DataTable();
-} );
+    $('#example').DataTable({
+        language: {
+            processing: "Obrađujem...",
+            search: "Pretraži",
+            lengthMenu: "Prikaži _MENU_ rezultata po stranici",
+            info: "Prikazano _START_ do _END_ od _TOTAL_ rezultata",
+            infoEmpty: "Prikazano 0 do 0 od 0 rezultata",
+            infoFiltered: "(filtrirano iz _MAX_ ukupnih rezultata)",
+            infoPostFix: "",
+            loadingRecords: "Učitavam...",
+            zeroRecords: "Ništa nije pronađeno",
+            emptyTable: "Nema podataka u tabeli",
+            paginate: {
+                first: "Prva",
+                previous: "Nazad",
+                next: "Naprijed",
+                last: "Zadnja"
+            },
+            aria: {
+                sortAscending: ": aktiviraj za rastući poredak",
+                sortDescending: ": aktiviraj za padajući poredak"
+            }
+        }
+    });
+});
