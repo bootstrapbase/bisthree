576d8512fcad7c5fc285f7912c05d6a5b028321e
sizif <imsirovic.ajdin@yahoo.com>
2017-03-23 13:01:19 +0000

    Add an attempt at organizing offers ponude

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -144,3 +144,40 @@ $(function() {
         changeYear: true
     });
 });
+
+
+// Expand all offers
+    $(function() {
+      $('a[data-toggle="collapse"]').on('click', function() {
+
+        var objectID = $(this).attr('href');
+
+        if ($(objectID).hasClass('in')) {
+          $(objectID).collapse('hide');
+        }
+
+        else {
+          $(objectID).collapse('show');
+        }
+      });
+
+
+      $('#expandAll').on('click', function() {
+
+        $('a[data-toggle="collapse"]').each(function() {
+          var objectID = $(this).attr('href');
+          if ($(objectID).hasClass('in') === false) {
+            $(objectID).collapse('show');
+          }
+        });
+      });
+
+      $('#collapseAll').on('click', function() {
+
+        $('a[data-toggle="collapse"]').each(function() {
+          var objectID = $(this).attr('href');
+          $(objectID).collapse('hide');
+        });
+      });
+
+    });
