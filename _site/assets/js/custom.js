5eb3d7b15a42ecfdc67c7faf4852064a4079f7b7
sizif <imsirovic.ajdin@yahoo.com>
2017-03-23 10:17:53 +0000

    Recreate all the top-level html pages

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -1,3 +1,8 @@
+/*
+https://www.sitepoint.com/working-jquery-datatables/
+
+*/
+
 $(document).ready(function() {
     $('#example').DataTable({
         language: {
@@ -21,6 +26,121 @@ $(document).ready(function() {
                 sortAscending: ": aktiviraj za rastući poredak",
                 sortDescending: ": aktiviraj za padajući poredak"
             }
+        },
+        "aaData": [
+            ["RSG Radio d.o.o. Sarajevo", "Rashladna vitrina marke Beko, zapremine 200 litara, nova", "5", "2017-04-05", "Marketing"],
+            ["ASA Group d.o.o. Sarajevo", "Uredski sto, iverica, sa dodatkom najmanje 4 ladice", "2", "2017-05-05", "Prerada drveta"],
+            ["Luk d.o.o. Tuzla", "Uredske stolice, crne, statične, bez naslonjača", "1", "2017-05-04", "Industrija namještaja"],
+            ["Perfect d.o.o. Tuzla", "Novo putničko vozilo marke Škoda ili VW, sa klimom", "1", "2017-06-07", "Rent-a-car"],
+            ["Bicom d.o.o. Tuzla", "Papir za štampu, format A4, pakovanje 500 listova", "4", "2017-04-05", "IT industrija"],
+        ],
+        "aoColumnDefs": [{
+            "sTitle": "Naziv firme",
+            "aTargets": ["firm_name"]
+        }, {
+            "aTargets": [1],
+            // "bSortable": false,
+            "bSortable": true,
+            "mRender": function(url, type, full) {
+                // return '<a href="' + url + '">' + url + '</a>';
+                return '<a href="detaljan-oglas.html">' + url + '</a>';
+            }
+        },
+        /*
+        {
+            "aTargets": [3],
+            "sType": "date",
+            "mRender": function(date, type, full) {
+                return (full[2] == "Blog") ?
+                    new Date(date).toDateString() :
+                    "N/A";
+            }
         }
+        */
+        ]
+    });
+});
+
+/*
+$("#example").dataTable({
+  "aaData":[
+    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+    ["Flippa","http://flippa.com","Marketplace","null"],
+    ["99designs","http://99designs.com","Marketplace","null"],
+    ["Learnable","http://learnable.com","Online courses","null"],
+    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"]
+  ],
+  "aoColumnDefs":[{
+        "sTitle":"Site name"
+      , "aTargets": [ "site_name" ]
+  },{
+        "aTargets": [ 1 ]
+      , "bSortable": false
+      , "mRender": function ( url, type, full )  {
+          return  '<a href="'+url+'">' + url + '</a>';
+      }
+  },{
+        "aTargets":[ 3 ]
+      , "sType": "date"
+      , "mRender": function(date, type, full) {
+          return (full[2] == "Blog") 
+                    ? new Date(date).toDateString()
+                    : "N/A" ;
+      }  
+  }]
+});
+*/
+
+/*
+example2: 
+http://www.gyrocode.com/articles/jquery-datatables-alphabetical-search/
+*/
+$(document).ready(function(){
+   var table = $('#example2').DataTable({
+      ajax: 'arrays.txt',
+      dom: 'Alfrtip',
+      alphabetSearch: {
+         column: 0 
+      }
+   });
+});
+
+// https://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3
+// http://codepen.io/claviska/pen/vAgmd
+$(function() {
+
+    // We can attach the `fileselect` event to all file inputs on the page
+    $(document).on('change', ':file', function() {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    });
+
+    // We can watch for our custom `fileselect` event like this
+    $(document).ready(function() {
+        $(':file').on('fileselect', function(event, numFiles, label) {
+
+            var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+            if (input.length) {
+                input.val(log);
+            }
+            else {
+                if (log) alert(log);
+            }
+
+        });
+    });
+
+});
+
+// datepicker jquery ui
+$(function() {
+    $(".date").datepicker({
+        dateFormat: 'dd/mm/yy',
+        changeMonth: true,
+        changeYear: true
     });
 });
